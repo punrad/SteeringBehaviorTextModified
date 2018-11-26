c08c4e800025d48fb7d5b32fe2140940197f40c6
punrad <34401702+punrad@users.noreply.github.com>
2018-11-26 10:52:34 +0000

    Removed irrelevant features and started on constructor.

--- SketchFolder/sketch.js
+++ SketchFolder/sketch.js
@@ -2,68 +2,25 @@
 // http://codingtra.in
 // Steering Text Paths
 // I added the functionality to make the particles change into another text and changed the positioning of the text to always be in the middle of the canvas
-var fontReady = false;
+
 var font;
 var vehicles = [];
 
-var texts = ['Velkommen', 'til', 'Line', 'og', 'Martines', 'designmøte'];
+var texts = ['Velkommen'];
 var nextT = 0;
 var maxChangeForce = 20;
 
 var instructions = [];
 var insText = '';
 
-function preload()
-{
-    font = loadFont('BlackHanSans-Regular.ttf', fontRead);
+function preload() {
+    font = loadFont('https://punrad.github.io/SteeringBehaviorTextModified/SketchFolder/TTWPGOTT.ttf');
 }
 
 function setup()
 {
-    if(fontReady == true)
-    {
-
-      createCanvas(1200, 400);
-      background(0);
-
-      var bounds = font.textBounds(texts[nextT], 0, 0, 192);
-      var posx = width / 2 - bounds.w / 2;
-      var posy = height / 2 + bounds.h / 2;
-
-      var points = font.textToPoints(texts[nextT], posx, posy, 192, {
-          sampleFactor: 0.1
-      });
-
-      for (var i = 0; i < points.length; i++) {
-          var pt = points[i];
-          var vehicle = new Vehicle(pt.x, pt.y);
-          vehicles.push(vehicle);
-      }
-
-      var boundsIns = font.textBounds(insText, 0, 0, 30);
-      var posxIns = width / 2 - boundsIns.w / 2;
-      var posyIns = height / 6 + boundsIns.h / 2;
-
-      var insAr = split(insText, ' ');
-
-      for (var i = 0; i < insAr.length; i++) {
-          var bounds2 = font.textBounds(insAr[i], 0, 0, 30);
-          var posx2 = posxIns;
-          var posy2 = posyIns;
-
-          posxIns += bounds2.w + 10;
-
-          var points2 = font.textToPoints(insAr[i], posx2, posy2, 30, {
-              sampleFactor: 0.1
-          });
-
-          for (var j = 0; j < points2.length; j++) {
-              var pt = points2[j];
-              var v = new Vehicle(pt.x, pt.y, 3);
-              instructions.push(v);
-          }
-      }
-  }
+    createCanvas(1200, 400);
+    textPointSetup();
 }
 
 function draw() {
@@ -84,62 +41,42 @@ function draw() {
     }
 }
 
-function mouseClicked() {
-    nextT++;
-    if (nextT > texts.length - 1) {
-        nextT = 0;
-    }
-
-    var bounds = font.textBounds(texts[nextT], 0, 0, 192);
-    var posx = width / 2 - bounds.w / 2;
-    var posy = height / 2 + bounds.h / 2;
-
-    var points = font.textToPoints(texts[nextT], posx, posy, 192, {
-        sampleFactor: 0.1
-    });
-
-    if (points.length < vehicles.length) {
-        var toSplice = vehicles.length - points.length;
-        vehicles.splice(points.length - 1, toSplice);
-
-        for (var i = 0; i < points.length; i++) {
-            vehicles[i].target.x = points[i].x;
-            vehicles[i].target.y = points[i].y;
-
-            var force = p5.Vector.random2D();
-            force.mult(random(maxChangeForce));
-            vehicles[i].applyForce(force);
-        }
-    } else if (points.length > vehicles.length) {
-
-        for (var i = vehicles.length; i < points.length; i++) {
-            var v = vehicles[i - vehicles.length].clone();
-
-            vehicles.push(v);
-        }
-
-        for (var i = 0; i < points.length; i++) {
-            vehicles[i].target.x = points[i].x;
-            vehicles[i].target.y = points[i].y;
-
-            var force = p5.Vector.random2D();
-            force.mult(random(maxChangeForce));
-            vehicles[i].applyForce(force);
-        }
-
-    } else {
-        for (var i = 0; i < points.length; i++) {
-            vehicles[i].target.x = points[i].x;
-            vehicles[i].target.y = points[i].y;
-
-            var force = p5.Vector.random2D();
-            force.mult(random(maxChangeForce));
-            vehicles[i].applyForce(force);
-        }
-    }
-}
-
-function fontRead()
+function textPointSetup()
 {
-    fontReady = true;
+  var bounds = font.textBounds(texts[0], 0, 0, 192);
+  var posx = width / 2 - bounds.w / 2;
+  var posy = height / 2 + bounds.h / 2;
+
+  var points = font.textToPoints(texts[0], posx, posy, 192, { sampleFactor: 0.1 });
+
+  for (var i = 0; i < points.length; i++)
+  {
+      var pt = points[i];
+      var vehicle = new Vehicle(pt.x, pt.y);
+      vehicles.push(vehicle);
+  }
+
+  var boundsIns = font.textBounds(insText, 0, 0, 30);
+  var posxIns = width / 2 - boundsIns.w / 2;
+  var posyIns = height / 6 + boundsIns.h / 2;
+
+  var insAr = split(insText, ' ');
+
+  for (var i = 0; i < insAr.length; i++)
+  {
+      var bounds2 = font.textBounds(insAr[i], 0, 0, 30);
+      var posx2 = posxIns;
+      var posy2 = posyIns;
+
+      posxIns += bounds2.w + 10;
+
+      var points2 = font.textToPoints(insAr[i], posx2, posy2, 30, { sampleFactor: 0.1 });
+
+      for (var j = 0; j < points2.length; j++)
+      {
+          var pt = points2[j];
+          var v = new Vehicle(pt.x, pt.y, 3);
+          instructions.push(v);
+      }
+  }
 }
